b2370962bca250408b91e1578860b2760ade507c
wsierra010 <wsierra010@gmail.com>
2020-12-17 10:21:53 +0100

    Create first Article #11

--- mailBox.js
+++ mailBox.js
@@ -1,4 +1,26 @@
 const mails = document.getElementById('mailbox');
 const mailArticle = document.createElement('article');
-mailArticle.setAttribute('id','mails');
-mailArticle.
+
+// Create Article in MailBox
+mailArticle.setAttribute('id','mail');
+mailArticle.classList.add('main__mailArticle');
+mailArticle.classList.add('col-xs-12');
+
+// Add this Article in Section mailbox
+mails.appendChild(mailArticle);
+
+// Create content in mailArticle
+const titleArticle = document.createElement('h2');
+const paragraphArticle = document.createElement('p');
+
+// Clases and ID
+titleArticle.classList.add('text');
+paragraphArticle.classList.add('text');
+
+// TextContent
+titleArticle.textContent = 'Assembler School BCN';
+paragraphArticle.textContent = 'Wednesday: Join us at "From 0 to product, a decision-driven path to success"'
+
+// Add content in Article
+mailArticle.appendChild(titleArticle);
+mailArticle.appendChild(paragraphArticle);
