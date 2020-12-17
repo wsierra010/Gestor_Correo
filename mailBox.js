0c2315ac6331a447fc71710392366a8822130023
wsierra010 <wsierra010@gmail.com>
2020-12-17 12:44:37 +0100

    JavaScript

--- mailBox.js
+++ mailBox.js
@@ -1,5 +1,7 @@
 const mails = document.getElementById('mailbox');
 const mailArticle = document.createElement('article');
+// Array para guardar todos los correos
+let ContMailBox = [];
 
 // Create Article in MailBox
 mailArticle.setAttribute('id','mail');
@@ -10,17 +12,41 @@ mailArticle.classList.add('col-xs-12');
 mails.appendChild(mailArticle);
 
 // Create content in mailArticle
+const checkBox = document.createElement('input');
+const star = document.createElement('input');
+const fixed = document.createElement('input');
 const titleArticle = document.createElement('h2');
 const paragraphArticle = document.createElement('p');
 
-// Clases and ID
+// checkbox
+checkBox.setAttribute('type','checkbox');
+checkBox.setAttribute('id','checked');
+// star favorites checkbox
+star.setAttribute('type','checkbox');
+star.setAttribute('id','favorites');
+// fixed checkbox
+fixed.setAttribute('type','checkbox');
+fixed.setAttribute('id','fixed');
+// Add Classes
 titleArticle.classList.add('text');
 paragraphArticle.classList.add('text');
 
 // TextContent
-titleArticle.textContent = 'Assembler School BCN';
-paragraphArticle.textContent = 'Wednesday: Join us at "From 0 to product, a decision-driven path to success"'
+titleArticle.textContent = 'Daniel Jimenez Prats';
+paragraphArticle.textContent = 'Te enviamos este correo para decir que Daniel Jimenez Prats es un máquina'
 
 // Add content in Article
+mailArticle.appendChild(checkBox);
+mailArticle.appendChild(star);
+mailArticle.appendChild(fixed);
 mailArticle.appendChild(titleArticle);
 mailArticle.appendChild(paragraphArticle);
+
+
+console.log(mailArticle);
+
+function pruebas(){
+    for(i=0; i<50; i++){
+        mailArticle.innerHTML;
+    }
+}
