61469a1d9127bb1be4dbb12edfd31e3549d54b43
wsierra010 <wsierra010@gmail.com>
2020-12-21 17:47:47 +0100

    Clean Code

--- mailBox.js
+++ mailBox.js
@@ -1,41 +1,40 @@
 const mails = document.getElementById('mailbox');
 const mainContainer = document.getElementById('main__container')
-// Array para guardar todos los correos
-
+// Array que guarda los mails en tipo objeto
+let ContMailBox = [];
 
 
 function articleMail(to, subject, textcontent){
+    // Create general container
     const mailArticle = document.createElement('article');
-    // Create Article in MailBox
+    // Add Clases and Id in MailArticle
     mailArticle.setAttribute('id','mail');
     mailArticle.classList.add('main__mailArticle');
     mailArticle.classList.add('col-xs-12');
 
-    // Add this Article in Section mailbox
-    // mails.appendChild(mailArticle);
-
     // Create content in mailArticle
     const checkboxes = document.createElement('figure');
-    const checkBox = document.createElement('input');
-    const star = document.createElement('input');
-    const fixed = document.createElement('input');
+        const checkBox = document.createElement('input');
+        const star = document.createElement('input');
+        const fixed = document.createElement('input');
+    // Create figures for put the information mails
     const toTitle = document.createElement('figure');
     const subjectMain = document.createElement('figure');
     const textareaFooter = document.createElement('figure');
 
     // Add classes in checkboxes figure
     checkboxes.setAttribute('class','col-xs-1');
-    // checkbox
-    checkBox.setAttribute('type','checkbox');
-    checkBox.setAttribute('id','checked');
-    // star favorites checkbox
-    star.setAttribute('type','checkbox');
-    star.setAttribute('id','favorites');
-    star.setAttribute('checked','true');
-    star.classList.add('star');
-    // fixed checkbox
-    fixed.setAttribute('type','checkbox');
-    fixed.setAttribute('id','fixed');
+        // checkbox
+        checkBox.setAttribute('type','checkbox');
+        checkBox.setAttribute('id','checked');
+        // star favorites checkbox
+        star.setAttribute('type','checkbox');
+        star.setAttribute('id','favorites');
+        star.setAttribute('checked','true');
+        star.classList.add('star');
+        // fixed checkbox
+        fixed.setAttribute('type','checkbox');
+        fixed.setAttribute('id','fixed');
 
 
     // Clases and Atributtes in Mails Content
@@ -46,35 +45,30 @@ function articleMail(to, subject, textcontent){
     textareaFooter.setAttribute('id','third');
     textareaFooter.setAttribute('class','col-xs-5');
 
-    // Add content in Article
+    // Add checkbox in first figure
     checkboxes.appendChild(checkBox);
     checkboxes.appendChild(star);
     checkboxes.appendChild(fixed);
+    // Add figures in MailArticle
     mailArticle.appendChild(checkboxes);
     mailArticle.appendChild(toTitle);
     mailArticle.appendChild(subjectMain);
     mailArticle.appendChild(textareaFooter);
 
+    // Asignation parameters function
     toTitle.innerHTML = to;
     subjectMain.innerHTML = subject;
     textareaFooter.innerHTML = textcontent;
 
-
     return mailArticle;
 }
 
-
-
-
 // Create Modal window compose
-
-let ContMailBox = [];
 const CreateModal = document.getElementById('compose');
-
 CreateModal.addEventListener('click', createCompose);
 
 function createCompose(){
-    //          Create Compose window Modal
+    // Create Compose window Modal
     let window = document.createElement('section');
     // Add content in Compose window section
     let headerCompose = document.createElement('div');
@@ -89,16 +83,18 @@ function createCompose(){
     window.appendChild(textareaCompose);
     window.appendChild(sentCompose);
 
-    // Add elements in headerCompose
+    // Add elements in HeaderCompose
     sentCompose.setAttribute('id','exit');
+    // Paragraph Text
     let paraf = document.createElement('p');
-    let icon = document.createElement('i');
+    paraf.setAttribute('class','textTitle')
     paraf.textContent = "New Message";
+    // Icon Fontawesome
+    let icon = document.createElement('i');
     icon.setAttribute('class','fas fa-times cross');
     icon.setAttribute('id','close');
-    paraf.setAttribute('class','textTitle')
 
-    // Add class list css
+    // Add ClassList in HeaderCompose
     headerCompose.classList.add('headerCompose');
 
     headerCompose.appendChild(paraf);
@@ -125,15 +121,13 @@ function createCompose(){
     sentCompose.classList.add('sendButton');
     sentCompose.setAttribute('id','send');
 
-
-
+    // Add Class CSS Style in Modal Window
     window.classList.add('modal');
 
+    // Add Modal Window in mainContainer
     mainContainer.appendChild(window);
 
-
-
-
+    // Function Close Modal Window
     const close = document.getElementById('close');
     close.addEventListener('click', exit);
 
@@ -141,15 +135,17 @@ function createCompose(){
         window.remove();
     }
 
+    // Function Create Object Mail and Send in mainContainer
     const sendButton = document.getElementById('send');
     sendButton.addEventListener('click', Mail);
 
-
+    // Function Create Object Mail
     function Mail(){
         const toInput = document.getElementById('toInput').value;
         const subjectInput = document.getElementById('subjectInput').value;
         const textareaInput = document.getElementById('textareaInput').value;
 
+        // Object
         MailContent={
             to: toInput,
             subject: subjectInput,
@@ -158,24 +154,29 @@ function createCompose(){
         // console.log(MailContent);
 
         capturar();
-
+        // Function push object in ContMail Box Array
         function capturar(){
             // console.log('Dani Jimenez Maquina')
             ContMailBox.push(MailContent);
         }
         // console.log(ContMailBox);
 
+        // Get Inputs information
         let first = document.getElementById('first');
         let second = document.getElementById('second');
         let third = document.getElementById('third');
 
+        // Close Window Modal After click Send
         window.remove();
+
+        // Empty mailBox Container
         mails.innerHTML = "";
+        // Create iterator Foreach every time an object is created
         ContMailBox.forEach(e => {
-            console.log(e.textcontent, e.subject, e.to);
+            // console.log(e.textcontent, e.subject, e.to);
             let variable = articleMail(e.to, e.subject, e.textcontent);
             mails.appendChild(variable);
-            console.log(variable);
+            // console.log(variable);
         });
     }
 
