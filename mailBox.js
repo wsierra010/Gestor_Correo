264602f93de257ade2fde0f3dea053a8daa16eda
wsierra010 <wsierra010@gmail.com>
2020-12-21 12:21:08 +0100

    Create Object Mails #14

--- mailBox.js
+++ mailBox.js
@@ -2,7 +2,6 @@ const mails = document.getElementById('mailbox');
 const mailArticle = document.createElement('article');
 const mainContainer = document.getElementById('main__container')
 // Array para guardar todos los correos
-let ContMailBox = [];
 
 
 
@@ -12,14 +11,19 @@ mailArticle.classList.add('main__mailArticle');
 mailArticle.classList.add('col-xs-12');
 
 // Add this Article in Section mailbox
-mails.appendChild(mailArticle);
+// mails.appendChild(mailArticle);
 
 // Create content in mailArticle
+const checkboxes = document.createElement('figure');
 const checkBox = document.createElement('input');
 const star = document.createElement('input');
 const fixed = document.createElement('input');
-// const titleArticle = document.createElement('h2');
-// const paragraphArticle = document.createElement('p');
+const toTitle = document.createElement('figure');
+const subjectMain = document.createElement('figure');
+const textareaFooter = document.createElement('figure');
+
+// Add classes in checkboxes figure
+checkboxes.setAttribute('class','col-xs-1');
 
 // checkbox
 checkBox.setAttribute('type','checkbox');
@@ -32,20 +36,24 @@ star.classList.add('star');
 // fixed checkbox
 fixed.setAttribute('type','checkbox');
 fixed.setAttribute('id','fixed');
-// Add Classes
-// titleArticle.classList.add('text');
-// paragraphArticle.classList.add('text');
 
-// TextContent
-// titleArticle.textContent = 'Daniel Jimenez Prats';
-// paragraphArticle.textContent = 'Te enviamos este correo para decir que Daniel Jimenez Prats es un máquina'
+
+// Clases and Atributtes in Mails Content
+toTitle.setAttribute('id','first');
+toTitle.setAttribute('class','col-xs-3');
+subjectMain.setAttribute('id','second');
+subjectMain.setAttribute('class','col-xs-3');
+textareaFooter.setAttribute('id','third');
+textareaFooter.setAttribute('class','col-xs-5');
 
 // Add content in Article
-mailArticle.appendChild(checkBox);
-mailArticle.appendChild(star);
-mailArticle.appendChild(fixed);
-// mailArticle.appendChild(titleArticle);
-// mailArticle.appendChild(paragraphArticle);
+checkboxes.appendChild(checkBox);
+checkboxes.appendChild(star);
+checkboxes.appendChild(fixed);
+mailArticle.appendChild(checkboxes);
+mailArticle.appendChild(toTitle);
+mailArticle.appendChild(subjectMain);
+mailArticle.appendChild(textareaFooter);
 
 
 const CreateModal = document.getElementById('compose');
@@ -67,11 +75,14 @@ function createCompose(){
     window.appendChild(subjectCompose);
     window.appendChild(textareaCompose);
     window.appendChild(sentCompose);
+
     // Add elements in headerCompose
     let paraf = document.createElement('p');
     let icon = document.createElement('i');
     paraf.textContent = "New Message";
     icon.setAttribute('class','fas fa-times cross');
+    icon.setAttribute('id','close');
+    paraf.setAttribute('class','textTitle')
 
     // Add class list css
     headerCompose.classList.add('headerCompose');
@@ -81,24 +92,66 @@ function createCompose(){
 
     // Add AppendChild in destinedCompose
     destinedCompose.setAttribute('placeholder','To');
+    destinedCompose.setAttribute('id','toInput');
     destinedCompose.classList.add('toInput');
 
-    // Add AppendChild in AffairCompose
+    // Add AppendChild in SubjectCompose
     subjectCompose.setAttribute('placeholder','Subject');
+    subjectCompose.setAttribute('id','subjectInput');
     subjectCompose.classList.add('subjectInput')
 
     // Add lists in TextArea
     textareaCompose.classList.add('textareaCompose');
+    textareaCompose.setAttribute('id','textareaInput');
 
     // Button send
     let name = document.createElement('p');
     name.textContent = "Send"
     sentCompose.appendChild(name);
     sentCompose.classList.add('sendButton');
+    sentCompose.setAttribute('id','send');
 
 
 
     window.classList.add('modal');
 
     mainContainer.appendChild(window);
+
+
+    const sendButton = document.getElementById('send');
+    sendButton.addEventListener('click', Mail);
+
+
+    function Mail(){
+        const toInput = document.getElementById('toInput').value;
+        const subjectInput = document.getElementById('subjectInput').value;
+        const textareaInput = document.getElementById('textareaInput').value;
+
+            MailContent={
+            to: toInput,
+            subject: subjectInput,
+            textcontent: textareaInput
+        }
+        console.log(MailContent);
+
+        let ContMailBox = [];
+        capturar();
+
+        function capturar(){
+            // console.log('Dani Jimenez Maquina')
+            ContMailBox.push(MailContent);
+        }
+        console.log(ContMailBox);
+
+        let first = document.getElementById('first');
+        let second = document.getElementById('second');
+        let third = document.getElementById('third');
+
+        first.innerHTML += MailContent.to;
+        second.innerHTML += MailContent.subject;
+        third.innerHTML += MailContent.textcontent;
+    }
 }
+
+
+
